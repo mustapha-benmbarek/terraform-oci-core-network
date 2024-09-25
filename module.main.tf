3cf73c4574fdd13274101f008d8c450ac8ab0b60
Mustapha Benmbarek <mustapha.benmbarek@gmail.com>
2024-09-25 18:18:42 +0400

    Initial commit

--- module.main.tf
+++ module.main.tf
@@ -13,21 +13,21 @@ module "vcns" {
   core-compartments = module.compartments.ids
 }
 
-/*Networking Module | VCN Subnet
+/*Networking Module | VCN Subnet*/
 module "vcn-subnets" {
   source            = "./modules/vcn-subnet"
   core-vcns         = module.vcns.ids
   core-compartments = module.compartments.ids
   core-vcn-subnets  = local.lst-vcn-subnets
-}*/
+}
 
-/*Networking Module | VCN Internet Gateway
+/*Networking Module | VCN Internet Gateway*/
 module "vcn-internet-gateways" {
   source                     = "./modules/vcn-internet-gateway"
   core-vcns                  = module.vcns.ids
   core-compartments          = module.compartments.ids
   core-vcn-internet-gateways = local.lst-vcn-internet-gateways
-}*/
+}
 
 /*Networking Module | VCN NAT Gateway
 module "vcn-nat-gateways" {
